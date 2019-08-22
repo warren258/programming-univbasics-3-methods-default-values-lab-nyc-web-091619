6fca55e9906604b96db09aad00641950c022362d
Warren McDonald <warrenjmac@mail.com>
2019-08-22 23:26:21 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 # Your code here
-def meal_choice (veg1, veg2, protien = "meat")
+def meal_choice (veg1, veg2, protein = "meat")
   puts "Whats a nutritous meal!"
   puts "A plate of #{protine} with #{veg1} and #{veg2}."
 end
